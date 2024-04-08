1eeef7239fbc22572034433f7dff615ceda34214
Marianne <bruyantmarianne@gmail.com>
2024-04-08 21:02:02 +0200

    WIP

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 14:07:33 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/04/08 16:34:19 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/04/08 17:10:20 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,25 @@
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
 
+# define ALLOWED_BASE "01NSEW"
+
+typedef enum path_txt
+{
+	NO,
+	SO,
+	WE,
+	EA
+};
+
 typedef struct s_data
 {
 	t_map	*map;
 	t_point	*position;
+	char	path_texture[4];
+	char	floor_c;
+	char	ceiling_c;
+	void	*mlx_ptr;
+	void	*win_ptr;
 }	t_data;
 
 #endif
