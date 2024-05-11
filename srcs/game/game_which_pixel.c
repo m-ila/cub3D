82357051734c02db45afcd211c0bfa5de9d5cf59
Marianne <bruyantmarianne@gmail.com>
2024-05-11 20:46:54 +0200

    almost there

--- srcs/game/game_which_pixel.c
+++ srcs/game/game_which_pixel.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 14:59:53 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/05/11 20:17:28 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/05/11 20:43:51 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	ft_get_x_offset(t_segment *seg, t_img_mlx *img)
 	int	offset;
 
 	if (seg->horizontal_hit)
-		offset = (seg->until.y % TILE_SIZE) * img->width / 64;
+		offset = (seg->until.y % TILE_SIZE) * img->width / TILE_SIZE;
 	else
-		offset = (seg->until.x % TILE_SIZE) * img->width / 64;
+		offset = (seg->until.x % TILE_SIZE) * img->width / TILE_SIZE;
 	if (offset >= img->width)
 		offset = img->width - 1;
 	return (offset);
@@ -44,11 +44,10 @@ int	ft_get_pixel(t_data *cub, t_segment *seg, int i)
 	int		offset;
 	//int		scale;
 
-
 	pix.x = ft_get_x_offset(seg, cub->imgs[seg->direction]);
 	pix.y = ft_get_y_offset(seg, cub->imgs[seg->direction], i);
-	offset = (pix.y * cub->imgs[seg->direction]->width + pix.x) * \
-	(pix.y * cub->imgs[seg->direction]->bpp / 8);
+	//offset = (pix.y * cub->imgs[seg->direction]->width + pix.x) * (pix.y * cub->imgs[seg->direction]->bpp / 8);
+	offset = pix.x * (cub->imgs[seg->direction]->bpp / 8) + pix.y * cub->imgs[seg->direction]->s_line;
 	if (offset >= 0 && offset < cub->imgs[seg->direction]->s_line * cub->imgs[seg->direction]->height)
 		color = ((unsigned char) cub->imgs[seg->direction]->adress[offset] << 16) |\
 	 ((unsigned char) cub->imgs[seg->direction]->adress[offset + 1] << 8) | \
