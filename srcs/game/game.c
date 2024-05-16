af1e8b6d2b9e35dc0bbb33c9606c51fae8b642cb
Marianne BRUYANT <mbruyant@made-f0Br3s7.clusters.42paris.fr>
2024-05-16 12:27:00 +0200

    added hook press and release

--- srcs/game/game.c
+++ srcs/game/game.c
@@ -13,7 +13,9 @@ void ft_game(t_data *cub)
     render_2d_map(cub);
     render_3d(cub);
 
-    mlx_key_hook(cub->win_3d, key_hook, cub);
+    //mlx_hook(win_ptr, 2, 1L << 0, key_pressed, &keycode);
+    mlx_hook(cub->win_3d, 2, 1L << 0 ,key_hook, cub);
+    mlx_hook(cub->win_3d, 3, 1L << 1 ,key_hook, cub);
     mlx_mouse_hook(cub->win_3d, ft_handle_clic, cub);  // Mouse interaction for 2D view
     mlx_loop(cub->mlx_ptr);
 }
