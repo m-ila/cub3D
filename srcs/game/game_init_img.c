e7ad491ae7657e3c8ff55ec8cb64ce3753b0fdb1
Marianne BRUYANT <mbruyant@made-f0Br3s7.clusters.42paris.fr>
2024-05-17 12:03:38 +0200

    norm

--- srcs/game/game_init_img.c
+++ srcs/game/game_init_img.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 16:43:30 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/05/17 11:58:35 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/05/17 12:03:30 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,26 @@ bool	ft_free_imgs(t_data *cub, int count, bool return_val)
 	return (return_val);
 }
 
-
-bool ft_open_images(t_data *cub)
+bool	ft_open_images(t_data *cub)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    cub->imgs = ft_calloc(4, sizeof(t_img_mlx *));
-    if (!cub->imgs)
-        return (ft_free_imgs(cub, i, true));
-    while (i < 4)
-    {
-        cub->imgs[i] = ft_calloc(1, sizeof(t_img_mlx));
-        if (!cub->imgs[i]) {
-            return (ft_free_imgs(cub, i, true));
-        }
-        cub->imgs[i]->img = mlx_xpm_file_to_image(cub->mlx_ptr, \
-        cub->path_texture[i], &cub->imgs[i]->width, &cub->imgs[i]->height);
-        if (!cub->imgs[i]->img) {
-            return (ft_free_imgs(cub, i + 1, true));
-        }
-        cub->imgs[i]->adress = mlx_get_data_addr(cub->imgs[i]->img, \
-        &cub->imgs[i]->bpp, &cub->imgs[i]->s_line, &cub->imgs[i]->endian);
-        i++;
-    }
-    return (true);
+	i = 0;
+	cub->imgs = ft_calloc(4, sizeof(t_img_mlx *));
+	if (!cub->imgs)
+		return (ft_free_imgs(cub, i, true));
+	while (i < 4)
+	{
+		cub->imgs[i] = ft_calloc(1, sizeof(t_img_mlx));
+		if (!cub->imgs[i])
+			return (ft_free_imgs(cub, i, true));
+		cub->imgs[i]->img = mlx_xpm_file_to_image(cub->mlx_ptr, \
+		cub->path_texture[i], &cub->imgs[i]->width, &cub->imgs[i]->height);
+		if (!cub->imgs[i]->img)
+			return (ft_free_imgs(cub, i + 1, true));
+		cub->imgs[i]->adress = mlx_get_data_addr(cub->imgs[i]->img, \
+		&cub->imgs[i]->bpp, &cub->imgs[i]->s_line, &cub->imgs[i]->endian);
+		i++;
+	}
+	return (true);
 }
-
