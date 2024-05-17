eedf425b02d419f21055a409a0eeedc6d669b2b2
Marianne BRUYANT <mbruyant@made-f0Br3s7.clusters.42paris.fr>
2024-05-17 13:06:42 +0200

    includes norme ok

--- inc/def_mlx.h
+++ inc/def_mlx.h
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 14:37:49 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/05/11 16:52:27 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/05/17 13:06:33 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # define R_CLIC 3
 # define LEFT_ARROW 65361
 # define RIGHT_ARROW 65363
+
 typedef struct s_img_mlx
 {
 	void	*img;
